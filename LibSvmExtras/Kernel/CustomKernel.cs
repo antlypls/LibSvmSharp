b65079e975df644c91ce02d31a09ce6a69bda5be
Anatoliy Plastinin <anatoliy.plastinin@gmail.com>
2010-12-18 16:37:21 +0300

    add custom kernel

---
+++ LibSvmExtras/Kernel/CustomKernel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LibSvm;
+
+namespace LibSvmExtras.Kernel
+{
+  public sealed class CustomKernel<T> : KernelBase<T>
+  {
+    private Func<T, T, double> _func = null;
+
+    public CustomKernel(Func<T, T, double> func)
+    {
+      _func = func;
+    }
+
+    internal override void FillParameters(SvmParameter<T> param)
+    {
+      param.KernelFunc = _func;
+    }
+  }
+}
